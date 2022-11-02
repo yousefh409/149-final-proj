f7256479724c37945d51ffad72a5be2e35ddef63
DragonKyro <kyle31412@gmail.com>
2022-11-02 16:42:08 -0700

    accel + flex

--- glove/glove.ino
+++ glove/glove.ino
@@ -8,6 +8,13 @@
 //https://www.okdo.com/getting-started/get-started-with-arduino-nano-33-ble/
 
 #include <ArduinoBLE.h>
+#include <Wire.h>
+#include <LSM303.h>
+
+const int flexPin = A0;
+
+LSM303 compass;
+char report[80];
 
 BLEService gloveService("fff0");  // User defined service
 
@@ -19,9 +26,17 @@ BLEIntCharacteristic finger2("ccc2", BLERead | BLENotify);
 BLEIntCharacteristic finger3("ccc3", BLERead | BLENotify);
 BLEIntCharacteristic finger4("ccc4", BLERead | BLENotify);
 
+BLEIntCharacteristic accelx("aaa1", BLERead | BLENotify);
+BLEIntCharacteristic accely("aaa2", BLERead | BLENotify);
+BLEIntCharacteristic accelz("aaa3", BLERead | BLENotify);
+
 void setup() {
   Serial.begin(9600);    // initialize serial communication
   while (!Serial);
+
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin
 
@@ -37,6 +52,10 @@ void setup() {
   gloveService.addCharacteristic(finger2);
   gloveService.addCharacteristic(finger3);
   gloveService.addCharacteristic(finger4);
+
+  gloveService.addCharacteristic(accelx);
+  gloveService.addCharacteristic(accely);
+  gloveService.addCharacteristic(accelz);
   BLE.addService(gloveService); // Add service
 
   BLE.advertise();  // Start advertising
@@ -59,6 +78,21 @@ void loop() {
     int count = 0;
     while (central.connected()){
       delay(250);
+      Serial.println("precompass read");
+      compass.read();
+      Serial.println("postcompass read");
+      accelx.setValue(int(compass.a.x));
+      accely.setValue(int(compass.a.y));
+      accelz.setValue(int(compass.a.z));
+      snprintf(report, sizeof(report), "A: %6d %6d %6d    M: %6d %6d %6d",
+          compass.a.x, compass.a.y, compass.a.z,
+          compass.m.x, compass.m.y, compass.m.z);
+      Serial.println(report);
+
+      int flex = analogRead(flexPin);
+      Serial.println(flex);
+      finger1.setValue(int(flex));
+
       gloveCharacteristic.setValue(count);
       count += 1;
     } // keep looping while connected
