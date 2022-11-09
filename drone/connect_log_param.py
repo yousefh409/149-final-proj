1547b6133790c67554eb87dc1049faec9fc0f4b9
Derick Tseng <dericktseng@tutanota.com>
2022-11-08 19:12:06 -0800

    start working on drone

--- drone/connect_log_param.py
+++ drone/connect_log_param.py
@@ -1,21 +1,72 @@
 # import logging
+import logging
+import sys
 import time
+from threading import Event
+import keyboard
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.commander import Commander
 
-# URI to the Crazyflie to connect to
-uri = 'radio://0/120/250K/E7E7E7E7E7'
 
-def simple_connect():
-    print("Yeah, I'm connected! :D")
-    time.sleep(3)
-    print("Now I will disconnect :'(")
+
+def getvelocity():
+    vx = 0.0
+    vy = 0.0
+    vz = 0.0
+    maxvel = 0.05
+
+    if keyboard.is_pressed('w'):
+        print('w pressed')
+        vx += 1
+    if keyboard.is_pressed('s'):
+        print('s pressed')
+        vx -= 1
+    if keyboard.is_pressed('a'):
+        print('a pressed')
+        vy -= 1
+    if keyboard.is_pressed('d'):
+        print('d pressed')
+        vy += 1
+    if keyboard.is_pressed('space'):
+        print('space pressed')
+        vz += 1
+    if keyboard.is_pressed('enter'):
+        print('enter pressed')
+        vz -= 1
+
+    v_squared = vx**2 + vy**2 + vz**2
+    tolerance = 1e-5
+    if (abs(v_squared) <= tolerance):
+        return (0.0, 0.0, 0.0)
+    else:
+        # TODO - fix this
+        return vx, vy, vz
+
+
+def command(cf: Crazyflie):
+    commander = Commander(cf)
+
+    while True:
+        if (keyboard.is_pressed('backspace')):
+            print('Quitting')
+            commander.send_stop_setpoint()
+            break
+        else:
+            vx, vy, vz = getvelocity()
+            commander.send_velocity_world_setpoint(vx, vy, vz, 0.0)
+            time.sleep(0.1)
+
     return None
 
+
 if __name__ == '__main__':
-    # Initialize the low-level drivers
+    # URI to the Crazyflie to connect to
+    uri = 'radio://0/10/250K/E7E7E7E7E7'
+    deck_attached_event = Event()
+    logging.basicConfig(level=logging.ERROR)
     cflib.crtp.init_drivers()
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        simple_connect()
+        command(scf.cf)
